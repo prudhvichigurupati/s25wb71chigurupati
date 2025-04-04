3bb10be5ddf0e3ef422a3fd5e6b7d544a7e75536
prudhvichigurupati <prudhvichigurupati88@gmail.com>
2025-04-03 21:29:25 -0500

    new changes

--- routes/auras.js
+++ routes/auras.js
@@ -3,15 +3,7 @@ var router = express.Router();
 
 /* GET auras page. */
 router.get('/', function(req, res, next) {
-  // Define the results array in the backend
-  var results = [
-    { color: "Indigo", intensity: 8, emotion_associated: "Clarity" },
-    { color: "Green", intensity: 6, emotion_associated: "Calm" },
-    { color: "Red", intensity: 9, emotion_associated: "Passion" }
-  ];
-
-  // Pass the results array to the view
-  res.render('auras', { title: 'Search Results - Auras', results: results });
+  res.render('auras', { title: 'Search Results - Auras' });
 });
 
 module.exports = router;
